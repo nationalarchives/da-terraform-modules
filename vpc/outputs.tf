8decd533845dec3634581adcaef14ec2223b7c7f
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-06-05 13:31:02 +0100

    Output the route table IDs

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,6 +2,10 @@ output "private_subnets" {
   value = aws_subnet.private.*.id
 }
 
+output "private_route_table_ids" {
+  value = merge(aws_route_table.private_nat_gateway.*.id, aws_route_table.private_nat_instance.*.id)
+}
+
 output "public_subnets" {
   value = aws_subnet.public.*.id
 }
