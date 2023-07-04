21a73fac6ac1f2f78cab84c7383541024280ae58
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-07-04 16:49:00 +0100

    Add GetKeyPolicy

--- kms/main.tf
+++ kms/main.tf
@@ -53,7 +53,7 @@ data "aws_iam_policy_document" "key_policy" {
       type        = "AWS"
       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
     }
-    actions   = ["kms:DescribeKey"]
+    actions   = ["kms:DescribeKey", "kms:GetKeyPolicy"]
     resources = ["*"]
   }
   statement {
