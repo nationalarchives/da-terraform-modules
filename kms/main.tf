06502e0aa4ce485f5673f27e539418f78b4363a8
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-07-04 13:38:52 +0100

    Add DescribeKey for account root.
    This way, we can view encrypted queues and topics without being able to
    see the contents.

--- kms/main.tf
+++ kms/main.tf
@@ -49,6 +49,14 @@ data "aws_iam_policy_document" "key_policy" {
   statement {
     principals {
       type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:DescribeKey"]
+    resources = ["*"]
+  }
+  statement {
+    principals {
+      type        = "AWS"
       identifiers = [module.kms_admin_role.role_arn]
     }
     actions = [
