bf4832d11cc65be5c52ea8db4e75d21cdbdd19f6
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-08-14 16:42:03 +0100

    DR2-1145 feat: Add secondary indexes and sort keys to module.
    Originally the module couldn't add sort keys or global secondary
    indexes. I've changed it so you can now.

--- dynamo/variables.tf
+++ dynamo/variables.tf
@@ -12,10 +12,33 @@ variable "write_capacity" {
   default = 5
 }
 
-variable "hash_key" {}
+variable "hash_key" {
+  type = object({ type = string, name = string })
+}
 
-variable "hash_key_type" {}
+variable "range_key" {
+  type    = object({ type = optional(string), name = optional(string) })
+  default = {}
+}
+
+variable "additional_attributes" {
+  type    = list(object({ type = string, name = string }))
+  default = []
+}
 
 variable "common_tags" {
   default = {}
+}
+
+variable "global_secondary_indexes" {
+  type = list(object({
+    name               = string
+    hash_key           = string
+    range_key          = optional(string)
+    write_capacity     = optional(number)
+    read_capacity      = optional(number)
+    projection_type    = string
+    non_key_attributes = optional(list(string), [])
+  }))
+  default = []
 }
