9d53cddeb4692779f499f19af7eca80789f938fb
TomJKing <tom.king@nationalarchives.gov.uk>
2023-08-30 09:59:36 +0100

    Pull request feedback

--- apigateway/outputs.tf
+++ apigateway/outputs.tf
@@ -2,6 +2,6 @@ output "api_url" {
   value = "https://${aws_api_gateway_rest_api.rest_api.id}.execute-api.${var.region}.amazonaws.com/${var.environment}"
 }
 
-output "api_arn" {
+output "api_execution_arn" {
   value = aws_api_gateway_rest_api.rest_api.execution_arn
 }
