32132b493efb76f6688a8d154dac5a2320feef16
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-09-07 16:04:42 +0100

    Ran terraform fmt

--- eventbridge_api_destination_rule/main.tf
+++ eventbridge_api_destination_rule/main.tf
@@ -1,19 +1,19 @@
 resource "aws_cloudwatch_event_rule" "rule" {
-  name        = var.name
-  description = var.description
+  name          = var.name
+  description   = var.description
   event_pattern = var.event_pattern
 }
 
 module "api_destination_policy" {
-  source = "../iam_policy"
-  name   = "${var.name}-policy"
-  policy_string = templatefile("${path.module}/templates/api_destination_policy.json.tpl", {api_destination_arn = var.api_destination_arn})
+  source        = "../iam_policy"
+  name          = "${var.name}-policy"
+  policy_string = templatefile("${path.module}/templates/api_destination_policy.json.tpl", { api_destination_arn = var.api_destination_arn })
 }
 
 module "api_destination_role" {
-  source = "../iam_role"
+  source             = "../iam_role"
   assume_role_policy = templatefile("${path.module}/templates/events_assume_role.json.tpl", {})
-  name = "${var.name}-role"
+  name               = "${var.name}-role"
   policy_attachments = {
     api_destination_policy = module.api_destination_policy.policy_arn
   }
@@ -21,17 +21,17 @@ module "api_destination_role" {
 }
 
 resource "aws_cloudwatch_event_target" "target" {
-  target_id         = var.name
-  arn               = var.api_destination_arn
-  role_arn          = module.api_destination_role.role_arn
-  rule              = aws_cloudwatch_event_rule.rule.name
-  input             = var.input
-  input_path        = var.input_path
+  target_id  = var.name
+  arn        = var.api_destination_arn
+  role_arn   = module.api_destination_role.role_arn
+  rule       = aws_cloudwatch_event_rule.rule.name
+  input      = var.input
+  input_path = var.input_path
   dynamic "input_transformer" {
     for_each = var.input_transformer == null ? [] : [var.input_transformer]
     content {
       input_template = input_transformer.value.input_template
-      input_paths = input_transformer.value.input_paths
+      input_paths    = input_transformer.value.input_paths
     }
   }
 }
