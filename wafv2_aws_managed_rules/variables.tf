0c1162478510be1991599b380c01eed1ae8e2118
TomJKing <tom.king@nationalarchives.gov.uk>
2023-12-11 14:17:42 +0000

    Run 'terraform fmt' command

--- wafv2_aws_managed_rules/variables.tf
+++ wafv2_aws_managed_rules/variables.tf
@@ -10,12 +10,12 @@ variable "arn_associations" {
 
 variable "aws_managed_rules" {
   description = "List of AWS managed rules to be applied"
-  type        = list(object({
-    name = string
-    priority = number
-    managed_rule_group_statement_name = string
+  type = list(object({
+    name                                     = string
+    priority                                 = number
+    managed_rule_group_statement_name        = string
     managed_rule_group_statement_vendor_name = string
-    metric_name = string
+    metric_name                              = string
   }))
   default = [
     {
