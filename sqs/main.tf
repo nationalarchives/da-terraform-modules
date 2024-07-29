df66e966dfd951c4f806b91ab907a69701feea9c
sparkhi <4743002+sparkhi@users.noreply.github.com>
2024-07-29 11:21:42 +0100

    syntax error correction

--- sqs/main.tf
+++ sqs/main.tf
@@ -77,7 +77,6 @@ resource "aws_cloudwatch_metric_alarm" "dlq_metric_messages_visible_alarm" {
   dimensions = {
     QueueName = "${var.queue_name}-dlq"
   }
-  notification_topic = var.dlq_notification_topic
 }
 
 resource "aws_cloudwatch_metric_alarm" "dlq_metric_messages_sent_alarm" {
@@ -92,7 +91,6 @@ resource "aws_cloudwatch_metric_alarm" "dlq_metric_messages_sent_alarm" {
   dimensions = {
     QueueName = "${var.queue_name}-dlq"
   }
-  notification_topic = var.dlq_notification_topic
 }
 
 module "queue_cloudwatch_alarm" {
