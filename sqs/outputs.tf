b89a3dbb346da0e9a9c4e35dc7b27a14c66d5fdf
sparkhi <4743002+sparkhi@users.noreply.github.com>
2024-07-29 11:30:07 +0100

    Updated output variables

--- sqs/outputs.tf
+++ sqs/outputs.tf
@@ -14,10 +14,6 @@ output "dlq_sqs_url" {
   value = local.sqs_dlq.url
 }
 
-output "dlq_cloudwatch_alarm_arn" {
-  value = aws_cloudwatch_metric_alarm.dlq_metric_alarm.arn
-}
-
 output "dlq_cloudwatch_message_visible_alarm_arn" {
   value = aws_cloudwatch_metric_alarm.dlq_metric_messages_visible_alarm.arn
 }
