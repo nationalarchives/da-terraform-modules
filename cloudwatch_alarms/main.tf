3491fc5114cde161f7b4c1e54b57949971ad8e09
Anthony Hashemi <ant.hashemi@gmail.com>
2024-10-14 15:42:03 +0100

    Add terrafor provider def to the cloudwatch_alarms module

--- cloudwatch_alarms/main.tf
+++ cloudwatch_alarms/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.6.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "cloudwatch_metric_alarm" {
   alarm_name          = var.name
   comparison_operator = var.comparison_operator
