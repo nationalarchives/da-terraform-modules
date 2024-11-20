8c40e76457040ab28c89e1d8ce888c839c902530
Kurtis Mash <24705116+kurtismash@users.noreply.github.com>
2024-11-20 16:29:29 +0000

    DR2-1775 Allow IAM to manage KMS read permissions

--- kms/main.tf
+++ kms/main.tf
@@ -49,12 +49,16 @@ data "aws_iam_policy_document" "key_policy" {
     }
   }
   statement {
-    sid = "AccountRootDescribeKey"
+    sid = "EnableIamUserPermissionsReadOnly"
     principals {
       type        = "AWS"
       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
     }
-    actions   = ["kms:DescribeKey", "kms:GetKeyPolicy"]
+    actions = [
+      "kms:Describe*",
+      "kms:Get*",
+      "kms:List*"
+    ]
     resources = ["*"]
   }
   statement {
