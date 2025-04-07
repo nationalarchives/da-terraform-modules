c5704beea945c9434f0b7a8e1750a941d31b042e
techncl <nigel.lutale@nationalarchives.gov.uk>
2025-04-07 12:02:40 +0100

    Remove redundant outputs

--- sqs/outputs.tf
+++ sqs/outputs.tf
@@ -14,14 +14,6 @@ output "dlq_sqs_url" {
   value = local.sqs_dlq.url
 }
 
-output "dlq_cloudwatch_message_visible_alarm_arn" {
-  value = module.dlq_metric_messages_visible_alarm.cloudwatch_alarm_arn
-}
-
-output "queue_cloudwatch_message_visible_alarm_arn" {
-  value = module.queue_cloudwatch_alarm.cloudwatch_alarm_arn
-}
-
 output "alarms" {
   value = [module.queue_cloudwatch_alarm.cloudwatch_alarm_arn, module.dlq_metric_messages_visible_alarm.cloudwatch_alarm_arn]
 }
