1dbafd715982864d2ceef9761fa7cf50c629e103
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2025-05-20 09:41:28 +0100

    Deal with lists for DLQ resources

--- sqs/outputs.tf
+++ sqs/outputs.tf
@@ -7,17 +7,17 @@ output "sqs_queue_url" {
 }
 
 output "dlq_sqs_arn" {
-  value = local.sqs_dlq.arn
+  value = var.create_dlq ? local.sqs_dlq[0].arn : null
 }
 
 output "dlq_sqs_url" {
-  value = local.sqs_dlq.url
+  value = var.create_dlq ? local.sqs_dlq[0].url : null
 }
 
 output "alarms" {
-  value = [module.queue_cloudwatch_alarm.cloudwatch_alarm_arn, module.dlq_metric_messages_visible_alarm.cloudwatch_alarm_arn]
+  value = flatten([[module.queue_cloudwatch_alarm.cloudwatch_alarm_arn], module.dlq_metric_messages_visible_alarm.*.cloudwatch_alarm_arn])
 }
 
 output "event_alarms" {
-  value = [aws_cloudwatch_metric_alarm.new_messages_added_to_dlq_alert.arn]
+  value = aws_cloudwatch_metric_alarm.new_messages_added_to_dlq_alert.*.arn
 }
