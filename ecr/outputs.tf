d0cc0ce8339608518404759f7e9dee0f8bb50d9b
Rahat Ahmed <rahmed@zaizi.com>
2025-06-05 11:13:03 +0100

    adding ECR repo arn as a module output

--- ecr/outputs.tf
+++ ecr/outputs.tf
@@ -2,3 +2,8 @@ output "repository_url" {
   description = "URL of the ECR repository"
   value       = aws_ecr_repository.repository.repository_url
 }
+
+output "repository_arn" {
+  description = "ARN of the ECR repository"
+  value = aws_ecr_repository.repository.arn
+}
